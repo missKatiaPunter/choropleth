30d0826cc6c9557b0b3216f98d1892b10e05e71f
Katia <katia.punter@storyshareplatform.com>
2021-09-06 17:20:04 +0100

    Pass 2 tests"

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+let chartHeight = 600;
+let chartWidth = 800;
+// Appends the empty svg to the body of the document
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight)
